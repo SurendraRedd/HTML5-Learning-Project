1dc3f67d06bdc311a151941309a02ce034ca870d
SurendraRedd <37290660+SurendraRedd@users.noreply.github.com>
2018-09-25 10:37:35 +0530

    Add files via upload

---
+++ HTML5_Training/Day1/StructureDemo/styles/Day1/StructureDemo/scripts/index.js
@@ -0,0 +1,18 @@
+window.addEventListener('load',function ()
+{
+    //alert("loaded...");
+
+    var aRef=document.getElementById("register");
+
+    aRef.addEventListener('click', function()
+    {
+        window.open('register.html', 'regForm', 'width=800 height=400,top=200,left=200');
+
+    })
+
+
+
+
+
+
+})
